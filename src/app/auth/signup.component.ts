62610c01e90a23b5d6b46d8220a2129713943bbb
booblegum <booblegum42@gmail.com>
2017-02-17 16:39:45 +0700

    Add SASS remove Tests
    CSS --> SASS
    Bootstrap-4 добавлен через npm
    Удалены тесты (добавим, когда будем с ними работать)

--- src/app/auth/signup.component.ts
+++ src/app/auth/signup.component.ts
@@ -3,6 +3,6 @@ import { Component} from '@angular/core';
 @Component({
   selector: 'signup',
   templateUrl: `./signup.component.html`,
-  styleUrls: ['./auth.component.css'],
+  styleUrls: ['./auth.component.sass'],
 })
 export class SignupComponent { }
