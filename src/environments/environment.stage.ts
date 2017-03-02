73858388c8756a579324725b0b7f3e430d945269
Ilya Chistyakov <ilchistyakov@gmail.com>
2017-03-02 16:16:18 +0700

    add dockerfile

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -3,7 +3,7 @@
 export const environment = {
   production: false,
   api_url: 'http://stage.dircont.com:8888/api/v1',
-  google_captcha_key: '',
+  google_captcha_key: '6Ld9kxYUAAAAALwx6qmro6dnClKkASJTj-w3uzK-',
   skip_captcha: false,
   callback_url: 'http://stage.dircont.com:8888'
 };
