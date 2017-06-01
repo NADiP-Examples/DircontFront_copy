ca2ac306749446a54d7de4685aa66eace61d91c5
booblegum <booblegum42@gmail.com>
2017-06-01 15:13:35 +0700

    Profile
    Удалил лишние компоненты

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -6,11 +6,6 @@ import { Angular2FontAwesomeModule } from 'angular2-font-awesome/angular2-font-a
 
 import { SharedModule } from 'app/shared/shared.module';
 
-import { SelectStatusComponent } from './shared/components/select-status/select-status.component';
-import { UserDataComponent } from './shared/components/user-data/user-data.component';
-import { UserDataViewComponent } from './shared/components/user-data-view/user-data-view.component';
-import { CompanyDataComponent } from './shared/components/company-data/company-data.component';
-import { CompanyDataViewComponent } from './shared/components/company-data-view/company-data-view.component';
 import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
@@ -22,11 +17,6 @@ import { ProfileComponent } from './profile/profile.component';
     Angular2FontAwesomeModule
   ],
   declarations: [
-    SelectStatusComponent,
-    UserDataComponent,
-    UserDataViewComponent,
-    CompanyDataComponent,
-    CompanyDataViewComponent,
     ProfileComponent
   ]
 })
